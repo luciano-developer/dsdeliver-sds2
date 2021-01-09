36e27fbdbbece41dade91283150fba493b59925c
luciano-developer <lcoliveiraa@gmail.com>
2021-01-08 23:08:44 -0300

    GET /Orders

--- backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/repositories/ProductRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsdeliver.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-		List<Product> findAllByOrderNameAsc();
+		List<Product> findAllByOrderByNameAsc();
 }
