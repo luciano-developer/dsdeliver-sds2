913bd20c75747ed5634f936c44b4d8b877b72949
luciano-developer <lcoliveiraa@gmail.com>
2021-01-24 06:58:46 -0300

    add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Header() {
+  const navigation = useNavigation();
+  const handleOnPress = ()=>{
+    navigation.navigate('Home');
+  }
   return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png')} />
-      <Text style={styles.text}>DS Delivery</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleOnPress}>
+      <View style={styles.container}>
+        <Image source={require("../assets/logo.png")} />
+        <Text style={styles.text}>DS Delivery</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {    
-    backgroundColor: '#DA5C5C',
-    height:90,
-    paddingTop:50,
-    flexDirection:'row',
-    justifyContent:'center'
+  container: {
+    backgroundColor: "#DA5C5C",
+    height: 90,
+    paddingTop: 50,
+    flexDirection: "row",
+    justifyContent: "center",
   },
-  text:{
-    fontWeight: 'bold',
+  text: {
+    fontWeight: "bold",
     fontSize: 18,
     lineHeight: 25,
-    letterSpacing: -0.24,    
-    color: '#FFFFFF',
-    marginLeft:15,
-    fontFamily:'OpenSans_700Bold',
+    letterSpacing: -0.24,
+    color: "#FFFFFF",
+    marginLeft: 15,
+    fontFamily: "OpenSans_700Bold",
   },
 });
-
---
+++ front-mobile/src/Routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./Home";
+import Orders from "./Orders";
+
+export default function Routes() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        headerMode="none"
+        screenOptions={{ cardStyle: { backgroundColor: "#fff" } }}
+      >
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
